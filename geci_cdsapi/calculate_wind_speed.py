3e9d6b83e972c0ec81736b56c9e1ab96a7b3ab0a
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-06-26 21:21:53 +0000

    ✅ Pass tests

--- geci_cdsapi/calculate_wind_speed.py
+++ geci_cdsapi/calculate_wind_speed.py
@@ -1,6 +1,8 @@
 import xarray as xr
+import numpy as np
 
 
 def read_nc_file(nc_path):
     nc = xr.open_dataset(nc_path)
+    nc["wind_speed"] = np.sqrt(nc["u10"] ** 2 + nc["v10"] ** 2)
     return nc
