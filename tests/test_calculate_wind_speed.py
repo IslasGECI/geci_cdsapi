adabb286d3939ae9abbb924b3b4de54c18d052f4
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-06-26 21:20:40 +0000

    🛑🧪 Fail tests

--- tests/test_calculate_wind_speed.py
+++ tests/test_calculate_wind_speed.py
@@ -4,4 +4,4 @@ from geci_cdsapi.calculate_wind_speed import read_nc_file
 def test_nc_file():
     nc_path = "tests/data/era5_wind_sanbenito_2013.nc"
     obtained = read_nc_file(nc_path)
-    assert set(["u10", "v10"]) == set(list(obtained.keys()))
+    assert set(["u10", "v10", "wind_speed"]) == set(list(obtained.keys()))
