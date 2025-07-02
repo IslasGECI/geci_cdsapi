732147e02c304494ed14765f3a1278f076214150
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-07-02 20:50:08 +0000

    🛑🧪 Fail tests

--- tests/test_init_client.py
+++ tests/test_init_client.py
@@ -4,4 +4,4 @@ from geci_cdsapi.init_client import load_access_key
 def test_load_access_key():
     obtained = load_access_key()
     assert obtained is not None
-    assert len(obtained) == 35
+    assert len(obtained.split("-")) == 5
