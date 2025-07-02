72eaecfab11e8b8ef081657c4a4ae057164280d7
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-07-02 20:38:43 +0000

    ✅ Pass tests

--- geci_cdsapi/cli.py
+++ geci_cdsapi/cli.py
@@ -5,5 +5,15 @@ cli = typer.Typer()
 
 
 @cli.command()
+def monthly_wind_average(
+    start_year: int = typer.Option(),
+    end_year: int = typer.Option(),
+    island: str = typer.Option(),
+    output_path: str = typer.Option(),
+):
+    pass
+
+
+@cli.command()
 def version():
     return "0.1.0"
