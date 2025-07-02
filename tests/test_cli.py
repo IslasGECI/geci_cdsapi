35c596cf9835c3b54435eeed00dd8f0b53b50e2a
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-07-02 20:33:28 +0000

    🛑🧪 Fail tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -11,3 +11,17 @@ def test_version():
     result = runner.invoke(cli, ["--help"])
     assert result.exit_code == 0
     assert " version " in gtt.strip_ansi_sequences(result.stdout)
+
+
+def test_monthly_wind_average():
+    output_path = "tests/wind_average.csv"
+    gtt.if_exist_remove(output_path)
+    result = runner.invoke(
+        cli,
+        [
+            "monthly-wind-average",
+            "--output-path",
+            output_path,
+        ],
+    )
+    assert result.exit_code == 0
