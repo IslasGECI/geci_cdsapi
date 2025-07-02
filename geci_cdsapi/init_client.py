e5fed45f49b8d10e7ce31be712a84cb935753542
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-07-02 20:54:44 +0000

    ✅ Pass tests

--- geci_cdsapi/init_client.py
+++ geci_cdsapi/init_client.py
@@ -1,4 +1,10 @@
+import cdsapi
 import os
+
+
+def init_client():
+    client = cdsapi.Client(url="https://cds.climate.copernicus.eu/api", key=load_access_key())
+    return client
 
 
 def load_access_key():
