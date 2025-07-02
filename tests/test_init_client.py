91b23c8a02b040e91ba804992a4bc87ce9ef9701
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-07-02 20:53:11 +0000

    🛑🧪 Fail tests

--- tests/test_init_client.py
+++ tests/test_init_client.py
@@ -1,7 +1,13 @@
-from geci_cdsapi.init_client import load_access_key
+from geci_cdsapi.init_client import load_access_key, init_client
 
 
 def test_load_access_key():
     obtained = load_access_key()
     assert obtained is not None
     assert len(obtained.split("-")) == 5
+
+
+def test_init_client():
+    obtained = init_client()
+    expected_url = "https://cds.climate.copernicus.eu/api"
+    assert obtained.url == expected_url
