09e52545f93456b0a7c7bf541c2381cb9b0a25ab
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-07-02 18:32:48 +0000

    🛑🧪 Fail tests

--- tests/test_calculate_wind_speed.py
+++ tests/test_calculate_wind_speed.py
@@ -1,4 +1,4 @@
-from geci_cdsapi.calculate_wind_speed import calculate_wind_speed, read_nc_file
+from geci_cdsapi.calculate_wind_speed import calculate_wind_speed, read_nc_file, mean_by_month
 import xarray as xr
 import numpy as np
 
@@ -9,39 +9,45 @@ def test_nc_file():
     assert set(["u10", "v10", "wind_speed"]) == set(list(obtained.keys()))
 
 
+dataset = xr.Dataset(
+    {
+        "u10": (
+            ("valid_time", "latitude", "longitude"),
+            np.array(
+                [
+                    [[1, 2, 3, 4], [5, 6, 7, 8]],
+                    [[10, 20, 30, 40], [50, 60, 70, 80]],
+                    [[1, 2, 3, 4], [5, 6, 7, 8]],
+                ]
+            ),
+        ),
+        "v10": (
+            ("valid_time", "latitude", "longitude"),
+            np.array(
+                [
+                    [[5, 6, 7, 8], [1, 2, 3, 4]],
+                    [[1, 2, 3, 4], [5, 6, 7, 8]],
+                    [[10, 20, 30, 40], [50, 60, 70, 80]],
+                ]
+            ),
+        ),
+    },
+    coords={
+        "valid_time": np.array(["2023-01-01", "2023-01-02", "2024-01-03"], dtype="datetime64[ns]"),
+        "latitude": [10, 20],
+        "longitude": [30, 40, 50, 60],
+    },
+)
+
+
 def test_calculate_wind_speed():
-    dataset = xr.Dataset(
-        {
-            "u10": (
-                ("valid_time", "latitude", "longitude"),
-                np.array(
-                    [
-                        [[1, 2, 3, 4], [5, 6, 7, 8]],
-                        [[1, 2, 3, 4], [5, 6, 7, 8]],
-                        [[1, 2, 3, 4], [5, 6, 7, 8]],
-                    ]
-                ),
-            ),
-            "v10": (
-                ("valid_time", "latitude", "longitude"),
-                np.array(
-                    [
-                        [[5, 6, 7, 8], [1, 2, 3, 4]],
-                        [[1, 2, 3, 4], [5, 6, 7, 8]],
-                        [[1, 2, 3, 4], [5, 6, 7, 8]],
-                    ]
-                ),
-            ),
-        },
-        coords={
-            "valid_time": np.array(
-                ["2023-01-01", "2023-01-02", "2023-01-03"], dtype="datetime64[ns]"
-            ),
-            "latitude": [10, 20],
-            "longitude": [30, 40, 50, 60],
-        },
-    )
     obtained = calculate_wind_speed(dataset)
     assert set(["u10", "v10", "wind_speed"]) == set(list(obtained.keys()))
     obtained_wind_speed_first_day_and_position = obtained["wind_speed"][0, 0, 0].values
     assert np.sqrt(1**2 + 5**2) == obtained_wind_speed_first_day_and_position
+
+
+def test_mean_by_month():
+    obtained = mean_by_month(dataset)
+
+    assert len(obtained.u10) == 13
