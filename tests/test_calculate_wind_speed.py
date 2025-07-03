bc6731dfb51365f401b14afa8cb3fb0e8dfa764c
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-07-03 19:49:34 +0000

    🛑🧪 Fail tests

--- tests/test_calculate_wind_speed.py
+++ tests/test_calculate_wind_speed.py
@@ -53,6 +53,7 @@ def test_write_windspeed_dataset_to_csv():
     assert len(obtained) == expected_rows
     expected_columns = ["Índice", "Año", "Mes/Periodo", "Valor"]
     assert set(obtained.columns) == set(expected_columns)
+    assert obtained["Mes/Periodo"][0] == "Jan"
 
 
 def test_calculate_monthly_wind_speed():
